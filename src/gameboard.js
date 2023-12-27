28865a8285f0a499b4767efdfcea6e5df084ea54
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-28 01:49:40 +0300

    Refactor gameboard.js and export player in player.js

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,4 @@
-import Ship from "./ship";
+// import Ship from "./ship";
 
 function gameboard() {
   const board = Array(10);
--- src/player.js
+++ src/player.js
@@ -89,3 +89,5 @@ function player() {
     },
   };
 }
+
+export default player;

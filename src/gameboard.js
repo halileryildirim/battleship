b7bb2faaf17bda561d92041550af3fe5051173eb
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-27 03:47:51 +0300

    Add logic to prevent from ships overlap

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,30 +3,55 @@ import Ship from "./ship";
 function gameboard() {
   const board = Array(10);
   for (let i = 0; i < board.length; i += 1) {
-    board[i] = new Array(10);
+    const rows = Array(10);
+    board[i] = rows;
   }
 
   const ships = [];
 
-  function placeShip([row, col], shipSize) {
+  function placeShip([row, col], ship) {
     // update empty array elements as S when a ship placed
     // if placing S is impossible dont allow to place (out of boundaries or the element already is an S)
     // dont allow placement to nearby nodes  a 5,5 5,6 5,7 cant place to previous row, next row, next and previous column of the ship
     // ship index can be taken and update the index for the length of size to place and test if placeable but vertical and horizontal state is important
 
     // vertical ship placement
-    const ship = new Ship(shipSize);
+    const shipSize = ship.length;
     if (ship.vertical) {
       if (row + shipSize < 10 && board[row][col] !== "S") {
-        for (let i = row; i < shipSize; i += 1) {
-          board[i][col] = "S";
+        for (let i = 0; i < shipSize; i += 1) {
+          if (board[row + i][col] !== "S") board[row + i][col] = "S";
+          else {
+            while (i >= 0) {
+              delete board[row + i - 1][col];
+              i -= 1;
+            }
+            return "Ship deployment failed.";
+          }
         }
         ships.push(ship);
         return "Ship deployed successfuly.";
       }
       return "Ship deployment failed.";
     }
+
+    if (col + shipSize < 10 && board[row][col] !== "S") {
+      for (let i = 0; i < shipSize; i += 1) {
+        if (board[row][col + i] !== "S") board[row][col + i] = "S";
+        else {
+          while (i > 0) {
+            delete board[row][col + i - 1];
+            i -= 1;
+          }
+          return "Ship deployment failed.";
+        }
+      }
+      ships.push(ship);
+      return "Ship deployed successfuly.";
+    }
+    return "Ship deployment failed.";
   }
+
   return { board, ships, placeShip };
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,6 +1,8 @@
 import gameboard from "../gameboard";
+import Ship from "../ship";
 
 const board = gameboard();
+const carrier = new Ship(6);
 
 test("Gameboard to be array", () => {
   for (let i = 0; i < 9; i += 1) {
@@ -8,17 +10,37 @@ test("Gameboard to be array", () => {
   }
 });
 
+// Deploy ships, prevent to deploy on top of ships, check the amount of ships on gameboard after every attempt.
+
+// test("Vertical ship deployment", () => {
+//   expect(board.ships.length).toBe(0);
+//   expect(board.placeShip([0, 0], carrier)).toBe("Ship deployed successfuly.");
+//   expect(board.placeShip([0, 0], carrier)).toBe("Ship deployment failed.");
+//   expect(board.placeShip([0, 1], carrier)).toBe("Ship deployed successfuly.");
+//   expect(board.placeShip([0, 1], carrier)).toBe("Ship deployment failed.");
+//   expect(board.placeShip([2, 0], carrier)).toBe("Ship deployment failed.");
+//   expect(board.ships.length).toBe(2);
+// });
+
+// test("Horizontal ship deployment", () => {
+//   carrier.rotate();
+//   expect(board.ships.length).toBe(2);
+//   expect(board.placeShip([9, 1], carrier)).toBe("Ship deployed successfuly.");
+//   expect(board.placeShip([8, 0], carrier)).toBe("Ship deployed successfuly.");
+//   expect(board.ships.length).toBe(4);
+// });
+
 test("Ship deployment", () => {
-  // Deploy ships, prevent to deploy on top of ships, check the amount of ships on gameboard after every attempt.
-  const shipSize = 6;
-  expect(board.placeShip([0, 0], shipSize)).toBe("Ship deployed successfuly.");
-  expect(board.ships.length).toBe(1);
-  expect(board.placeShip([0, 0], shipSize)).toBe("Ship deployment failed.");
-  expect(board.ships.length).toBe(1);
-  expect(board.placeShip([0, 1], shipSize)).toBe("Ship deployed successfuly.");
-  expect(board.ships.length).toBe(2);
-  expect(board.placeShip([0, 1], shipSize)).toBe("Ship deployment failed.");
-  expect(board.ships.length).toBe(2);
-  expect(board.placeShip([2, 0], shipSize)).toBe("Ship deployment failed.");
-  expect(board.ships.length).toBe(2);
+  expect(board.ships.length).toBe(0);
+  carrier.rotate(); // horizontal deploy
+  expect(board.placeShip([8, 0], carrier)).toBe("Ship deployed successfuly.");
+  carrier.rotate(); // vertical deploy
+  expect(board.placeShip([0, 0], carrier)).toBe("Ship deployed successfuly.");
+  expect(board.placeShip([0, 0], carrier)).toBe("Ship deployment failed.");
+  expect(board.placeShip([3, 1], carrier)).toBe("Ship deployment failed."); // deploy start is not crossing with a ship but the final location is encountering a ship so cant be deployed
+  expect(board.placeShip([0, 6], carrier)).toBe("Ship deployed successfuly.");
+  carrier.rotate();
+  expect(board.placeShip([3, 1], carrier)).toBe("Ship deployment failed.");
+  expect(board.ships.length).toBe(3);
+  console.log(board.board);
 });
